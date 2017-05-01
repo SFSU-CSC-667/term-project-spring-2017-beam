f151d7ab26f4c3143b1e8154a06711baf46b93dd
Bruno Oliveira <bopandrade@gmail.com>
2017-05-01 02:38:48 +0000

    mvcish config

--- db/connection/index.js
+++ db/connection/index.js
@@ -1,4 +1,4 @@
 const pgp = require( 'pg-promise' )()
-const db = pgp( process.env.DATABASE_URL || 'postgres://jrob@localhost:5432/blargit-development' )
+const db = pgp( process.env.DATABASE_URL || 'postgres://group667:jkjkjk@localhost:5432/group667' )
 
 module.exports = db
