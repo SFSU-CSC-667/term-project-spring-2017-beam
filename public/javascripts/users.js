9d4591ed5f741571675e6623eecd2ead172288f9
Bruno Oliveira <bopandrade@gmail.com>
2017-05-02 05:06:21 +0000

    alerts

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -22,17 +22,23 @@ socket.on( 'redirect', ({destination}) => {
 socket.on ( 'lobby-chat', ({user_id, display_name, message}) => {
     const print = display_name + '#' + user_id+ ': ' + message
     console.log(print)
+    const chat_area = document.querySelector ( 'ul.chat_area' )
+    const append = `<td>${print}</td>`
+    chat_area.innerHTML += append
 })
 
 socket.on ( 'sucess', ({message}) => {
     console.log(message)
+    $.flash(message)
+    $(".alert-success").text(message).show().fadeTo(5000,0)
+
 })
 
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector( 'a.data' ).addEventListener( 'click', event => {
     event.preventDefault()
     event.stopPropagation()
-    const input = document.querySelector('input.chat_input').value
+    const input = 'lolmepops'
 
     socket.emit( 'data', {email: input})
   })
