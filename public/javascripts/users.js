4438748aa0b02cfee471765962d49996620fcac8
MarthaGomez <gomezlet@mail.sfsu.edu>
2017-05-04 17:37:52 -0700

    testing die buttons

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -224,25 +224,3 @@ document.addEventListener('DOMContentLoaded', function() {
   
   activateButtons();
 })
-
-
-function customRadio(radioName){
-  var radioButton = $('input[name="'+ radioName +'"]');
-  $(radioButton).each(function(){
-    $(this).wrap( "<span class='custom-radio'></span>" );
-    if($(this).is(':checked')){
-      $(this).parent().addClass("selected");
-    }
-  });
-  $(radioButton).click(function(){
-    if($(this).is(':checked')){
-      $(this).parent().addClass("selected");
-    }
-    $(radioButton).not(this).each(function(){
-      $(this).parent().removeClass("selected");
-    });
-  });
-}
-$(document).ready(function (){
-    customRadio("die");
-})
